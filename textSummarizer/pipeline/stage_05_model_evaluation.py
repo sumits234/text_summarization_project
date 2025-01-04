36a3918cf3b741c38dc356ec278b17c8c9301d18
sumits234 <22b0615@iitb.ac.in>
2025-01-04 20:26:57 +0530

    Update and rename textSummarizer/stage_05_model_evaluation.py to textSummarizer/pipeline/stage_05_model_evaluation.py

--- textSummarizer/pipeline/stage_05_model_evaluation.py
+++ textSummarizer/pipeline/stage_05_model_evaluation.py
@@ -2,13 +2,9 @@ from textSummarizer.config.configuration import ConfigurationManager
 from textSummarizer.conponents.model_evaluation import ModelEvaluation
 from textSummarizer.logging import logger
 
-
-
-
 class ModelEvaluationTrainingPipeline:
     def __init__(self):
         pass
-
     def main(self):
         config = ConfigurationManager()
         model_evaluation_config = config.get_model_evaluation_config()
